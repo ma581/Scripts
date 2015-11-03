ca19a2dad7e9a01d550f8858c81b7e658c1f20ab
ma581 <ma581@cam.ac.uk>
2015-11-03 15:41:23 +0000

    Initial

---
+++ Collision.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onCollisionEnter(Collision c)
+    {
+        //Called when there is a collision
+
+        Debug.Log(this.name + "Collided with " + c.gameObject.name);
+
+        if (c.gameObject.tag == "Player")
+        {
+
+        }
+
+        //var joint = gameobject.addcomponent<fixedjoint>();
+        //joint.connectedbody = c.rigidbody;
+    }
+}
