21d7cbcd95541a5a35104524764a10c1c7d034b4
ma581 <ma581@cam.ac.uk>
2015-11-04 10:30:13 +0000

    First Commit

---
+++ HorizontalMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    public float speed = 6.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update () {
+
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //Debug.Log("Input: " + moveHorizontal);
+
+        //var movement = new Vector3(0f, 0f, moveHorizontal * 10);
+        //transform.GetComponent<Rigidbody>().velocity = movement;
+
+        ////Lateral x
+        //float moveLateral = Input.GetAxis("Vertical");
+        //Debug.Log("Input: " + moveLateral);
+
+        //var movement2 = new Vector3(moveLateral * 10, 0f, 0f );
+        //transform.GetComponent<Rigidbody>().velocity = movement2;
+
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //moveDirection = transform.TransformDirection(moveDirection);
+        ////moveDirection *= speed;
+        //transform.GetComponent<Rigidbody>().velocity = moveDirection;
+	}
+
+    void FixedUpdate() {
+
+            var movement = new Vector3( Input.GetAxis("Horizontal") * 10, 
+                                        0,
+                                        Input.GetAxis("Vertical") * 10);
+            GetComponent<Rigidbody>().velocity = movement;
+           
+
+    }
+}
---
+++ StickyCube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickyCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log(this.name + "Collided with " + col.gameObject.name);
+            GetComponent<Rigidbody>().isKinematic = true; // stop physics
+            transform.parent = col.transform; // doesn't move yet, but will move w/what it hit
+        }
+    }
+}
