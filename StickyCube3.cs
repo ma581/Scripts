fc07d112d506e37d2fd226e7b5cb042a646dfb68
ma581 <ma581@cam.ac.uk>
2016-02-19 01:36:21 +0000

    PickingUpBricks

---
+++ StickyCube3.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickyCube3 : MonoBehaviour
+{
+
+    bool rightHandCollision = false;
+    bool leftHandCollision = false;
+    bool RTouchAndClosed = false;
+    bool LTouchAndClosed = false;
+
+    GameObject rightHand;
+    GameObject leftHand;
+    GameObject objectOrgParent;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (rightHandCollision == true && RUISSkeletonController.rightHandClosed == true)
+        {
+            Debug.Log("Both Hands are colliding");
+            RTouchAndClosed = true;
+            PickUpObject(rightHand);
+            Debug.Log("Picking up object!");
+
+
+        }
+        else {
+            Debug.Log("Both Hands are not colliding");
+            RTouchAndClosed = false;
+            DropObject();
+            Debug.Log("Dropping object!");
+
+        }
+
+
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log(this.name + "Collided with " + col.gameObject.name);
+        if (col.gameObject.name == "Bip001 R Hand")
+        {
+            Debug.Log(this.name + "Collided with " + col.gameObject.name);
+            rightHandCollision = true;
+            rightHand = col.gameObject;
+        }
+
+        if (col.gameObject.name == "Bip001 L Hand")
+        {
+            Debug.Log(this.name + "Collided with " + col.gameObject.name);
+            //GetComponent<Rigidbody>().isKinematic = true; // stop physics
+            //transform.parent = col.transform; // doesn't move yet, but will move w/what it hit
+
+            leftHandCollision = true;
+            leftHand = col.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+
+        if (col.gameObject.name == "Bip001 R Hand")
+        {
+            Debug.Log(this.name + " Stopped Collided with " + col.gameObject.name);
+            rightHandCollision = false;
+            
+
+
+        }
+
+        if (col.gameObject.name == "Bip001 L Hand")
+        {
+            Debug.Log(this.name + "Stopped Collided with " + col.gameObject.name);
+
+            leftHandCollision = false;
+
+        }
+
+    }
+
+
+
+
+   
+
+    void PickUpObject(GameObject hand)
+    {
+        Debug.Log("Pickup Method!");
+        //Debug.Log(gameObject.name);
+        GetComponent<Rigidbody>().isKinematic = true; // stop physics
+
+        transform.parent = hand.transform; // doesn't move yet, but will move w/what it hit
+                                           // Debug.Log(gameObject.name + "parent is " + transform.parent.name);
+
+    }
+
+    void DropObject()
+    {
+        Debug.Log("DropObject Method!");
+        GetComponent<Rigidbody>().isKinematic = false; // start physics
+        transform.parent = null; //unparent it
+    }
+
+}
+
---
+++ fistGesture.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fistGesture : MonoBehaviour
+{
+
+    //public GameObject FistScript;
+    //private RUISSkeletonController fistGestureRec;
+
+
+
+    //private RUISSkeletonManager.Skeleton.handState leftHandStatus, lastLeftHandStatus;
+    //private RUISSkeletonManager.Skeleton.handState rightHandStatus, lastRightHandStatus;
+    //public RUISSkeletonManager skeletonManager;
+    //bool l_state, r_state;
+
+    // Use this for initialization
+    void Start()
+    {
+        //fistGestureRec = FistScript.GetComponent<RUISSkeletonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+
+
+        if (RUISSkeletonController.leftHandClosed)
+        {
+            Debug.Log("LHand is Closed!");
+        }
+        else
+        {
+            Debug.Log("LHand is Open!");
+        }
+        if (RUISSkeletonController.rightHandClosed)
+        {
+            Debug.Log("RHand is Closed!");
+        }
+        {
+            Debug.Log("RHand is Open!");
+        }
+    }
+}
