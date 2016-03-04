5bd66bae85473c5fbeb511453b18a9b2faa969ed
ma581 <ma581@cam.ac.uk>
2016-03-04 17:15:39 +0000

    PickingUpObjectsCorrectly

---
+++ AreaTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AreaTrigger : MonoBehaviour {
+
+    public bool HandInRegion = false;
+    private bool toggle = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Bip001 R Hand")
+        {
+            HandInRegion = !HandInRegion;
+            Debug.Log("HAND IN HAMMER REGION");
+        }
+    }
+
+    
+}
---
+++ PickUpJoint.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpJoint : MonoBehaviour
+{
+
+    //public GameObject hand;
+    //Rigidbody rigidHand;
+    public GameObject HammerRegion; //Do  nothing 
+    private AreaTrigger hammerAreaTrig;
+
+    bool rightHandCollision = false; //Hand
+    bool rightFinger1COllision = false; //Finger
+    bool RTouchAndClosed = false;     //Touching and closed
+    bool Rholding = false;    //Holding
+    bool f = false;
+    public GameObject heldHammer;
+    public GameObject tableHammer;
+
+    void Start()
+    {
+        heldHammer.SetActive(false);
+        hammerAreaTrig = HammerRegion.GetComponent<AreaTrigger>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            f = !f;
+            heldHammer.SetActive(f);
+            tableHammer.SetActive(!f);
+        }
+
+        if (hammerAreaTrig.HandInRegion)
+        {
+            //f = !f;
+            heldHammer.SetActive(hammerAreaTrig.HandInRegion);
+            tableHammer.SetActive(!hammerAreaTrig.HandInRegion);
+        }
+
+    }
+
+
+}
